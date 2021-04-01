bda32974b36b745bb0a166f4e3f8ed482804647b
kkirby54 <sdk926@gmail.com>
2021-04-01 17:58:31 +0900

    플로이드 와샬

---
+++ 최단경로/2458 - 키 순서.cpp
@@ -0,0 +1,44 @@
+﻿#include <cstdio>
+#include <algorithm>
+
+const int INF = 100000000;
+int dist[501][501];
+
+int main() {
+	int N, M,a,b;
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			dist[i][j] = INF;
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &a,&b);
+		dist[a - 1][b - 1] = 1;
+	}
+
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+			}
+		}
+	}
+	int ret = 0;
+
+	for (int i = 0; i < N; i++) {
+		bool possible = true;
+		for (int j = 0; j < N; j++) {
+			if (i != j && dist[i][j] == INF && dist[j][i] == INF) {
+				possible = false;
+				break;
+			}
+		}
+		if (possible) {
+			ret++;
+		}
+	}
+	printf("%d", ret);
+}
---
+++ 최단경로/9205 - 맥주 마시면서 걸어가기.cpp
@@ -0,0 +1,51 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cmath>
+
+const int INF = 1000000000;
+
+int main() {
+	int T, N, r, c;
+	int dist[102][102];
+	scanf("%d", &T);
+	for (int t = 0; t < T; t++) {
+		scanf("%d", &N);
+		int x[102], y[102];
+
+		for (int i = 0; i < N + 2; i++) {
+			scanf("%d %d", &x[i], &y[i]);
+		}
+
+		for (int i = 0; i < N + 2; i++) {
+			for (int j = 0; j < N + 2; j++) {
+				int x_dis = (int) abs(x[i] - x[j]);
+				int y_dis = (int) abs(y[i] - y[j]);
+
+				if (x_dis + y_dis > 1000) dist[i][j] = INF;
+				else dist[i][j] = 1;
+			}
+		}
+
+		// 플로이드 와샬
+		for (int k = 0; k < N+2; k++) {
+			for (int i = 0; i < N + 2; i++) {
+				for (int j = 0; j < N + 2; j++)
+					if (dist[i][k] != INF && dist[k][j] != INF) dist[i][j] = 1;
+			}
+		}
+
+		if (dist[0][N + 1] == INF && dist[N + 1][0] == INF) {
+			printf("sad\n");
+		}
+		else printf("happy\n");
+
+		// x[0] y[0]이 출발.
+		// x[N+1], y[N+1]이 도착지점.
+
+		// 그걸 N개의 편의점을 이용해서 갈 수 있냐?
+
+
+
+	}
+
+}

91f53d59c64753c7fc1e22b0abb4d20e9eba32c9
kkirby54 <sdk926@gmail.com>
2021-05-28 22:07:40 +0900

    분할 정복은 그냥 다시 다 풀어야 될 듯

---
+++ Algospot/7장/울타리 잘라내기.cpp
@@ -0,0 +1,53 @@
+﻿#include <cstdio>
+#include <algorithm>
+
+int height[20001];
+int N;
+
+// [left, right)라고 하자
+int fence(int left, int right) {
+	if (left == right) return height[left];
+
+	int mid = (left + right) / 2;
+	int Max_val = std::max(fence(left, mid), fence(mid+1, right));
+
+	// cross의 경우를 따로 계산한다.
+	
+	// 처음 
+	int left_index = mid, right_index = mid+1;
+	int curr_height = std::min(height[left_index], height[right_index]);
+	Max_val = std::max(Max_val, 2 * curr_height);
+
+	// left나 right가 범위 내에 있으면 됨.
+	while (left < left_index || right_index < right) {
+		if (right_index < right && (left_index == left || height[left_index - 1] < height[right_index + 1])) {
+			right_index++;
+			curr_height = std::min(curr_height, height[right_index]);
+		}
+		else {
+			left_index--;
+			curr_height = std::min(curr_height, height[left_index]);
+		}
+
+		Max_val = std::max(Max_val, (right_index - left_index + 1) * curr_height);
+	}
+
+	
+	return Max_val;
+}
+
+int main() {
+	int C;
+	scanf("%d", &C);
+
+	for (int t = 0; t < C; t++) {
+		scanf("%d", &N);
+
+		for (int i = 0; i < N; i++) {
+			scanf("%d", height + i);
+		}
+
+		printf("%d\n", fence(0, N-1));
+	}
+
+}
---
+++ Algospot/7장/쿼드 트리 뒤집기.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverse(string::iterator& itr) {
+	char head = *itr;
+	++itr;
+
+	if (head == 'x') {
+		// 이러면 이제 바꿔야 하는데...
+		
+		string upperLeft = reverse(itr);
+		string upperRight = reverse(itr);
+		string lowerLeft = reverse(itr);
+		string lowerRight = reverse(itr);
+
+		return string("x") + lowerLeft + lowerRight +
+			upperLeft + upperRight;
+	}
+	else {
+		// 그냥 리턴함. 'b'든 'w'든
+		return string(1, head);
+	}
+
+}
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		string S;
+		cin >> S;
+
+		auto itr = S.begin();
+
+		string ans = reverse(itr);
+		cout << ans << "\n";
+
+	}
+}

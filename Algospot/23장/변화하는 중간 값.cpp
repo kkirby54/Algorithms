946c3eac8007ecf7aa6e0048fcf61ec29b33907e
kkirby54 <sdk926@gmail.com>
2021-06-17 01:10:27 +0900

    힙과 세그먼트 트리

---
+++ Algospot/23장/변화하는 중간 값.cpp
@@ -0,0 +1,58 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int C, N, a, b;
+long long curr;
+
+int main() {
+	scanf("%d", &C);
+
+	for (int t = 0; t < C; t++) {
+		scanf("%d %d %d", &N, &a, &b);
+
+		priority_queue<int> max_heap;
+		priority_queue<int, vector<int>, greater<int>> min_heap;
+
+		long long sum = 0;
+		// 조건
+		// 일단 최대힙에 작은 수들,
+		// 최소힙에 큰 수들이 들어간다.
+
+		// 최대힙에 하나가 더 들어간다.
+		// 홀수일 때 살펴보면.. 총 7개라 햇을때
+		// 최대힙에 4개, 최소힙에 3개가 들어간다.
+		// 그럼 그냥 최대힙의 top().
+
+		curr = 1983;
+		for (int i = 0; i < N; i++) {
+			// 무조건 최대힙 원소 <= 최소힙 원소
+			// max_heap.top() <= min_heap.top()
+			// max_heap.size()는 min_heap.size()보다 같거나 1 큼.
+
+			if (max_heap.size() == min_heap.size()) {
+				max_heap.push(curr);
+			}
+			else min_heap.push(curr);
+
+			// 이제 최대힙 원소 <= 최소힙 원소 조건 만족 확인
+			if (!max_heap.empty() && !min_heap.empty() &&
+				max_heap.top() > min_heap.top()) {
+				// 그럼 swap.
+				int a = max_heap.top(), b = min_heap.top();
+				max_heap.pop(), min_heap.pop();
+				max_heap.push(b); min_heap.push(a);
+			}
+
+			// 그리고 무조건 최대힙의 원소
+			sum += max_heap.top();
+			sum %= 20090711;
+			curr = (curr * a + b) % 20090711;
+		}
+
+		printf("%lld\n", sum);
+	}
+
+}
---
+++ Algospot/24장/등산로 - 댕청한 최대,최소 세그트리 직접 구현.cpp
@@ -0,0 +1,101 @@
+﻿#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+struct Segtree {
+	int N;
+	vector<int> RangeMax, RangeMin;
+
+	Segtree(const vector<int>& arr) {
+		// 원소의 개수
+		N = arr.size();
+		RangeMax.resize(N * 4);
+		RangeMin.resize(N * 4);
+
+		min_init(arr, 0, N - 1, 1);
+		max_init(arr, 0, N - 1, 1);
+	}
+
+	int min_init(const vector<int>& arr, int left, int right, int node) {
+		if (left == right) return RangeMin[node] = arr[left];
+
+		int mid = (left + right) / 2;
+		int leftMin = min_init(arr, left, mid, node * 2);
+		int rightMin = min_init(arr, mid + 1, right, node * 2 + 1);
+
+		return RangeMin[node] = min(leftMin, rightMin);
+	}
+	int max_init(const vector<int>& arr, int left, int right, int node) {
+		if (left == right) return RangeMax[node] = arr[left];
+
+		int mid = (left + right) / 2;
+		int leftMax = max_init(arr, left, mid, node * 2);
+		int rightMax = max_init(arr, mid + 1, right, node * 2 + 1);
+
+		return RangeMax[node] = max(leftMax, rightMax);
+	}
+
+	int query_min(int left, int right, int node, int nodeLeft, int nodeRight) {
+		// 교집합 없는 경우
+		if (nodeRight < left || right < nodeLeft) return INT_MAX;
+		
+		// nodeLeft-nodeRight가 쏙 포함되는 경우
+		// 이미 우리가 init 함수에서 해놧으니까 바로 리턴
+		if (left <= nodeLeft && nodeRight <= right) return RangeMin[node];
+
+		// 그 외의 경우는 걍 재귀
+		int mid = (nodeLeft + nodeRight) / 2;
+		return min(query_min(left, right, node * 2, nodeLeft, mid),
+			query_min(left, right, node * 2 + 1, mid + 1, nodeRight));
+	}
+	int query_max(int left, int right, int node, int nodeLeft, int nodeRight) {
+		// 교집합 없는 경우
+		if (nodeRight < left || right < nodeLeft) return 0;
+
+		// nodeLeft-nodeRight가 쏙 포함되는 경우
+		// 이미 우리가 init 함수에서 해놧으니까 바로 리턴
+		if (left <= nodeLeft && nodeRight <= right) return RangeMax[node];
+
+		// 그 외의 경우는 걍 재귀
+		int mid = (nodeLeft + nodeRight) / 2;
+		return max(query_max(left, right, node * 2, nodeLeft, mid),
+			query_max(left, right, node * 2 + 1, mid + 1, nodeRight));
+
+	}
+
+};
+
+int C, N, Q, a, b;
+
+int main() {
+	scanf("%d", &C);
+
+	for (int t = 0; t < C; t++) {
+		scanf("%d %d", &N, &Q);
+
+		vector<int> Arr(N);
+		for (int i = 0; i < N; i++) {
+			scanf("%d", &Arr[i]);
+		}
+
+		Segtree tree(Arr);
+		// 이제 여기서 min 처리, max 처리 완료된 Segtree가 존재한다.
+		// 그러면 쿼리에 대한 처리를 하려면.
+		
+		for (int i = 0; i < Q; i++) {
+			scanf("%d %d", &a, &b);
+			// 이 쿼리에 답한다.
+			int ret = 
+			tree.query_max(a, b, 1, 0, tree.N - 1)-tree.query_min(a, b, 1, 0, tree.N - 1);
+			printf("%d\n", ret);
+		}
+
+	}
+
+
+}
+
+

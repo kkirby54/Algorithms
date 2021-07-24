1be4b44e75dfc3a4900e086472d9601c987e7ae0
kkirby54 <sdk926@gmail.com>
2021-07-25 00:38:37 +0900

    MST까지

---
+++ Algospot/31장/근거리 네트워크.cpp
@@ -0,0 +1,98 @@
+﻿#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+const int INF = 987654321;
+using namespace std;
+typedef pair<int, int> P;
+
+struct Edge {
+	int u, v, dist;
+	Edge(int u=0, int v=0, int dist=0) : u(u), v(v), dist(dist) {}
+
+	bool operator<(const Edge& x) {
+		return dist < x.dist;
+	}
+};
+
+struct DisjointSet {
+	vector<int> parent, rank;
+
+	DisjointSet(int N) : parent(N) {
+		for (int i = 0; i < N; i++)
+			parent[i] = i;
+	}
+
+	int find(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = find(parent[u]);
+	}
+
+	void merge(int u, int v) {
+		u = find(u), v = find(v);
+
+		if (u == v) return;
+		parent[u] = v;
+	}
+
+};
+
+int C, N, M, x[500], y[500], a, b;
+
+// a건물, b건물 사이의 거리의 제곱을 구하는 함수
+int sqrDist(int a, int b) {
+	return (x[a] - x[b]) * (x[a] - x[b]) + (y[a] - y[b]) * (y[a] - y[b]);
+}
+
+Edge Edges[250000];
+
+int main() {
+	scanf("%d", &C);
+	for (int t = 0; t < C; t++) {
+		scanf("%d %d", &N, &M);
+		DisjointSet UF(N);
+		memset(Edges, 0, sizeof(Edges));
+
+		for (int i = 0; i < N; i++) scanf("%d", x + i);
+		for (int i = 0; i < N; i++) scanf("%d", y + i);
+
+		for (int i = 0; i < M; i++) {
+			// 이건 그냥 연결되어 있음.
+			// 그럼 UF로 둘을 그냥 묶어줘.
+			scanf("%d %d", &a, &b);
+			UF.merge(a,b);
+		}
+
+		int edgesNum = 0;
+
+		// 이제 간선들을 모아야한다.
+		for (int i = 0; i < N; i++) {
+			// 모든 edges를 만들어 본다.
+			for (int j = i+1; j < N; j++) {
+				if (UF.find(i) == UF.find(j)) continue;
+				Edges[edgesNum++] = Edge(i, j, sqrDist(i, j));
+			}
+		}
+
+		sort(Edges, Edges + edgesNum);
+
+		double ret = 0;
+		for (int i = 0; i < edgesNum; i++) {
+			Edge curr = Edges[i];
+
+			int rootU = UF.find(curr.u);
+			int rootV = UF.find(curr.v);
+
+			if (rootU == rootV) continue;
+
+			UF.merge(rootU, rootV);
+			ret += sqrt(curr.dist);
+		}
+
+		printf("%.10lf\n", ret);
+
+	}
+
+}
---
+++ Algospot/31장/여행 경로 정하기.cpp
@@ -0,0 +1,93 @@
+﻿#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+
+const int INF = 987654321;
+using namespace std;
+typedef pair<int, int> P;
+
+struct Edge {
+	int u, v, dist;
+	Edge(int u=0, int v=0, int dist=0) : u(u), v(v), dist(dist) {}
+
+	bool operator<(const Edge& x) {
+		return dist < x.dist;
+	}
+};
+
+struct DisjointSet {
+	vector<int> parent, rank;
+
+	DisjointSet(int N) : parent(N) {
+		for (int i = 0; i < N; i++)
+			parent[i] = i;
+	}
+
+	int find(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = find(parent[u]);
+	}
+
+	void merge(int u, int v) {
+		u = find(u), v = find(v);
+
+		if (u == v) return;
+		parent[u] = v;
+	}
+
+};
+
+int C, N, M, a, b, c;
+Edge edges[4000];
+
+// 하한을 edges[low].dist라고 했을 때,
+// Start-End를 잇는 경로의 상한을 구한다.
+int minUpperBound(int low) {
+	// 그렇게 되려면,
+	// 걍 low부터 N까지만을 이용해서 구해본다.
+
+	DisjointSet uf(N);
+
+	for (int i = low; i < M; i++) {
+		auto curr = edges[i];
+
+		int u = curr.u, v = curr.v;
+		if (uf.find(u) == uf.find(v)) continue;
+
+		uf.merge(u, v);
+
+		// 시작과 끝이 연결 되어있으면 ok.
+		if (uf.find(0) == uf.find(N - 1)) return curr.dist;
+	}
+
+	return INF;
+}
+
+int minWeightDifference() {
+	int ret = INF;
+
+	for (int i = 0; i < M; i++) {
+		ret = min(ret, minUpperBound(i) - edges[i].dist);
+	}
+	return ret;
+}
+
+
+int main() {
+	scanf("%d", &C);
+	for (int t = 0; t < C; t++) {
+		scanf("%d %d", &N, &M);
+
+		for (int i = 0; i < M; i++) {
+			scanf("%d %d %d", &a, &b, &c);
+			edges[i] = Edge(a, b, c);
+		}
+
+		sort(edges, edges + M);
+
+		printf("%d\n", minWeightDifference());
+
+	}
+}

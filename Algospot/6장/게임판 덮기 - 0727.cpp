da0395c30731564546b9f974294db1a3abe5d05c
kkirby54 <sdk926@gmail.com>
2021-07-28 15:25:08 +0900

    Brute-force 복습

---
+++ Algospot/6장/게임판 덮기 - 0727.cpp
@@ -0,0 +1,70 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int C, H, W, ret;
+char board[21][21];
+
+int dr[4][2] = { {1,0}, {0,1}, {1,1}, {1,1} };
+int dc[4][2] = { {0,1}, {1,1}, {0,1}, {0,-1} };
+
+void cover() {
+	int r = -1, c = -1;
+	// 가장 왼쪽 위에 있는 좌표 고른다.
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (board[i][j] == '.') {
+				r = i, c = j;
+				break;
+			}
+		}
+		if (r != -1) break;
+	}
+	// r이 -1이라면 다 채워진 경우임.
+	if (r == -1) {
+		ret++; return;
+	}
+
+	//printf("r = %d, c = %d에서 작업 중\n", r, c);
+	// 4가지 케이스를 다 본다.
+	for (int i = 0; i < 4; i++) {
+		int nR1 = r + dr[i][0];
+		int nR2 = r + dr[i][1];
+		int nC1 = c + dc[i][0];
+		int nC2 = c + dc[i][1];
+
+		// 이제 (n,r), (nR1, nC1), (nR2, nC2)
+		// 얘네가 다 .이어야지 할 수가 있음.
+		if (nR1 < 0 || nR1 >= H || nR2 < 0 || nR2 >= H) continue;
+		if (nC1 < 0 || nC1 >= W || nC2 < 0 || nC2 >= W) continue;
+
+		if (board[nR1][nC1] == '.' &&
+			board[nR2][nC2] == '.') {
+			board[r][c] = board[nR1][nC1] = board[nR2][nC2] = '#';
+			//printf("(%d,%d)에서 일단 처리: type=%d\n", r, c, i);
+			cover();
+			board[r][c] = board[nR1][nC1] = board[nR2][nC2] = '.';
+		}
+
+	}
+
+}
+
+
+int main() {
+	scanf("%d", &C);
+	for (int t = 0; t < C; t++) {
+		scanf("%d %d", &H, &W);
+		ret = 0;
+
+		for (int i = 0; i < H; i++)
+			scanf("%s", board[i]);
+
+		cover();
+		printf("%d\n", ret);
+
+	}
+
+}
---
+++ Algospot/6장/시계 맞추기 - 0727.cpp
@@ -0,0 +1,79 @@
+﻿#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int ButtonList[10][5] = {
+	{0,1,2,-1,-1},
+	{3,7,9,11,-1},
+	{4,10,14,15,-1},
+	{0,4,5,6,7},
+	{6,7,8,10,12},
+	{0,2,14,15,-1},
+	{3,14,15,-1,-1},
+	{4,5,7,14,15},
+	{1,2,3,4,5},
+	{3,4,5,9,13}
+};
+
+int C, ret;
+int clocks[16];
+
+// index번 버튼을 해결한다.
+// 안 누르거나, 1~3번 누르는 선택지.
+void Sync(int index, int cnt) {
+	if (index == 10) {
+		for (int i = 0; i < 16; i++)
+			if (clocks[i] != 0) return;
+		// 다 0이라면 12시랑 마찬가지임.
+		ret = min(ret, cnt);
+		return;
+	}
+
+	// 0번에서 3번까지 누를 수 있음.
+	int curr[5], curr_val[5];
+	for (int i = 0; i < 5; i++) {
+		curr[i] = ButtonList[index][i];
+
+		if (curr[i] != -1)
+			curr_val[i] = clocks[curr[i]];
+	}
+
+	for (int i = 0; i < 4; i++) {
+		// i번 돌린다는 얘기임.
+
+		for (int j = 0; j < 5; j++) {
+			if (ButtonList[index][j] == -1) continue;
+
+			//clock를 바꿔야함.
+			clocks[ButtonList[index][j]] = (clocks[ButtonList[index][j]] + i * 3) % 12;
+		}
+
+		Sync(index + 1, cnt + i);
+
+		// 이제 원래대로 돌려야 한다.
+		for (int j = 0; j < 5; j++) {
+			if (ButtonList[index][j] == -1) continue;
+			clocks[ButtonList[index][j]] = curr_val[j];
+		}
+
+	}
+
+
+}
+
+int main() {
+	scanf("%d", &C);
+	for (int t = 0; t < C; t++) {
+		for (int i = 0; i < 16; i++) {
+			scanf("%d", clocks + i);
+		}
+
+		ret = 987654321;
+
+		Sync(0, 0);
+
+		printf("%d\n", ret == 987654321 ? -1 : ret);
+	}
+
+}

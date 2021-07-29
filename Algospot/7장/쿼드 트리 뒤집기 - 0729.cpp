9cb81d9feaae50367c20dbce56ca13c80998707c
kkirby54 <sdk926@gmail.com>
2021-07-29 17:15:57 +0900

    char로 푸는 방법 찾다가 그냥 string 씀

---
+++ Algospot/7장/쿼드 트리 뒤집기 - 0729.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int C, len;
+string image;
+string::iterator itr;
+
+string reverse() {
+	char head = *itr;
+	// Base case
+	if (head != 'x') return string(1, head);
+
+	itr++; string upperLeft = reverse();
+	itr++; string upperRight = reverse();
+	itr++; string lowerLeft = reverse();
+	itr++; string lowerRight = reverse();
+
+	return 'x' + lowerLeft + lowerRight + upperLeft + upperRight;
+}
+
+
+int main() {
+	cin >> C;
+	for (int t = 0; t < C; t++) {
+		cin >> image;
+		itr = image.begin();
+
+		string ret = reverse();
+		cout << ret << "\n";
+	}
+
+}

b634cd5bf4551c6866af1df4116e999c76383392
kkirby54 <sdk926@gmail.com>
2021-12-23 00:40:43 +0900

    다양한 DP

---
+++ BOJ/DP/1256 - 사전(☆).cpp
@@ -0,0 +1,61 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+const int INF = 1000000000;
+int N, M, K;
+
+int dp[202][101][101];
+
+int dictionary(int pos, int n, int m) {
+	if (pos == 0) return 1;
+	if (n == 0 || m == 0) return 1; // 0이 되면 그 순간 다 정해짐.
+	
+	int& ret =  dp[pos][n][m];
+	if (ret != -1) return ret;
+
+	return ret = std::min(INF, dictionary(pos - 1, n - 1, m) +
+		dictionary(pos - 1, n, m - 1));
+}
+
+char ret[205];
+void findK(int pos, int n, int m, int k, int p) {
+	if (pos == 0) return;
+	if (n == 0) {
+		// 나머지는 다 m, 즉 'z'로 채우기
+		for (int i = 0; p + i < N + M; i++) {
+			ret[p + i] = 'z';
+		}
+		return;
+	}
+	if (m == 0) {
+		for (int i = 0; p + i < N + M; i++) {
+			ret[p + i] = 'a';
+		}
+		return;
+	}
+
+	int pivot = dictionary(pos-1, n-1, m);
+	if (pivot > k) {
+		ret[p] = 'a';
+		findK(pos - 1, n - 1, m, k, p+1);
+	}
+	else {
+		ret[p] = 'z';
+		findK(pos - 1, n, m - 1, k - pivot, p + 1);
+	}
+
+}
+
+int main() {
+	scanf("%d %d %d", &N, &M, &K);
+	memset(dp, -1, sizeof(dp));
+
+	//printf("%d\n", dictionary(N + M, N, M));
+	if (dictionary(N + M, N, M) < K) {
+		printf("-1");
+		return 0;
+	}
+
+	findK(N + M, N, M, K-1, 0);
+	printf("%s", ret);
+}
---
+++ BOJ/DP/2096 - 내려가기(☆).cpp
@@ -0,0 +1,34 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+
+int colMax[3], colMin[3], tempMax[3], tempMin[3];
+
+int main() {
+	int N, a, b, c;
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d %d", &a, &b,&c);
+
+		tempMax[0] = max(colMax[0], colMax[1]) + a;
+		tempMax[1] = max(colMax[2], max(colMax[0], colMax[1])) + b;
+		tempMax[2] = max(colMax[2], colMax[1]) + c;
+
+		tempMin[0] = min(colMin[0], colMin[1]) + a;
+		tempMin[1] = min(colMin[2], min(colMin[0], colMin[1])) + b;
+		tempMin[2] = min(colMin[2], colMin[1]) + c;
+
+		for (int j = 0; j < 3; j++) {
+			colMax[j] = tempMax[j];
+			colMin[j] = tempMin[j];
+		}
+	}
+
+	sort(colMax, colMax + 3);
+	sort(colMin, colMin + 3);
+	printf("%d %d\n", colMax[2],colMin[0]);
+	
+}

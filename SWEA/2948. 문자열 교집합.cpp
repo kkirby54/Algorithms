28359f74152277b348b8db4c1a9ff90ec0dceeb7
kkirby54 <sdk926@gmail.com>
2022-02-06 16:21:58 +0900

    해싱 + 문자열

---
+++ SWEA/2948. 문자열 교집합.cpp
@@ -0,0 +1,103 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+int T, N, M;
+char str[51];
+
+struct Node {
+	char key[51];
+	int data;
+	Node* next;
+
+	Node* alloc(char _key[], int _data, Node* _next) {
+		strcpy(key, _key);
+		data = _data;
+		next = _next;
+		return this;
+	}
+
+	// key가 없을 수도 있다.
+	Node* getPrevNode(char _key[]) {
+		Node* ptr = this;
+		while (ptr->next) {
+			if (strcmp(_key, ptr->next->key) == 0) {
+				return ptr;
+			}
+			ptr = ptr->next;
+		}
+		return nullptr; // 없다면 nullptr 반환.
+	}
+};
+
+int bCnt;
+Node buf[100000];
+Node hashTable[1 << 18];
+
+void init() {
+	bCnt = 0;
+	for (int i = 0; i < (1 << 18); i++)
+		hashTable[i].next = 0;
+}
+
+// str -> hashVal로 변환
+int getKey(char str[]) {
+	unsigned long long val = 5318;
+
+	for (int i = 0; str[i]; i++) {
+		val = ((val << 5) + val) + (str[i] - 'a' + 1);
+	}
+	return (int)(val % ((1 << 18) - 1));
+}
+
+int find(char str[]) {
+	int target = getKey(str);
+
+	Node* prevNode = hashTable[target].getPrevNode(str);
+	
+	if (prevNode == nullptr) {
+		//printf("%s는 없다\n", str);
+		return -1;
+	}
+
+
+	return prevNode->next->data;
+}
+
+void add(char str[], int data) {
+	int target = getKey(str);
+	hashTable[target].next = buf[bCnt++].alloc(str, data, hashTable[target].next);
+}
+
+int remove(char str[]) {
+	int target = getKey(str);
+	
+	Node* prevNode = hashTable[target].getPrevNode(str);
+	Node* targetNode = prevNode->next;
+
+	prevNode->next = targetNode->next;
+
+	return targetNode->data;
+}
+
+int main() {
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++) {
+		scanf("%d %d", &N, &M);
+		init();
+		int ret = 0;
+		for (int i = 0; i < N; i++) {
+			scanf("%s", str);
+
+			add(str, i);
+		}
+		for (int i = 0; i < M; i++) {
+			scanf("%s", str);
+			if (find(str) != -1) {
+				ret++;
+			}
+		}
+		printf("#%d %d\n", t, ret);
+
+	}
+}
---
+++ SWEA/4038. 단어가 등장하는 횟수.cpp
@@ -0,0 +1,55 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+char B[500001];
+char S[100001];
+int fail[100001];
+
+int T;
+
+int main() {
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++) {
+		scanf(" %s", B);
+		getchar();
+		scanf(" %s", S);
+		int ret = 0;
+		int N = strlen(B);
+		int M = strlen(S);
+		// 일단 S의 failure 배열을 채운다.
+		for (int i = 0; i < M; i++)
+			fail[i] = 0;
+
+		for (int i = 1, j = 0; i < M; i++) {
+			while (j > 0 && S[i] != S[j]) {
+				j = fail[j - 1];
+			}
+
+			if (S[i] == S[j])
+				fail[i] = ++j;
+		}
+
+		// KMP 알고리즘 적용
+		for (int i = 0, j = 0; i < N; i++) {
+			while (j > 0 && B[i] != S[j]) {
+				j = fail[j - 1];
+			}
+
+			// 같다면,
+			if (B[i] == S[j]) {
+				if (j == M - 1) {
+					//printf("i = %d에서 같\n", i);
+					ret++;
+					j = fail[j];
+				}
+				else j++;
+			}
+		}
+
+		printf("#%d %d\n", t, ret);
+
+	}
+
+}

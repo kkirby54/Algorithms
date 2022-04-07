20460f2560bcf5c0eb541ee2012e516b8c7439d4
kkirby54 <sdk926@gmail.com>
2022-04-08 01:34:40 +0900

    분할정복 Re

---
+++ Algospot/7장/울타리 잘라내기 - 22.04.08.cpp
@@ -0,0 +1,59 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+int C, N, H[20005];
+
+// [start, end]까지 최대를 구한다.
+int fence(int start, int end) {
+	if (start == end) return H[start];
+	if (start > end) return 0;
+
+	int mid = (start + end) / 2;
+
+	// 뚫고 들어가는 max를 구하자.
+	int lo = mid;
+	int hi = mid + 1;
+	int minHeight = std::min(H[lo], H[hi]);
+	int maxVal = 2 * minHeight;
+
+	while (lo >= start && hi <= end) {
+		int p = (start <= lo - 1) ? H[lo - 1] : -1;
+		int q = (hi + 1 <= end) ? H[hi + 1] : -1;
+
+
+		if (p == q == -1) break;
+		
+		if (p >= q) {
+			lo--;
+			minHeight = std::min(minHeight, p);
+		}
+		else if (p < q) {
+			hi++;
+			minHeight = std::min(minHeight, q);
+		}
+
+		// 실제 값 갱신
+		maxVal = std::max(maxVal, (hi - lo + 1) * minHeight);
+	}
+
+	int ret = std::max(fence(start, mid), fence(mid + 1, end));
+	ret = std::max(ret, maxVal);
+
+	return ret;
+}
+
+int main() {
+	scanf("%d", &C);
+
+	while (C--) {
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			scanf("%d", H + i);
+		}
+
+		int ans = fence(0, N - 1);
+		printf("%d\n", ans);
+	}
+
+}
---
+++ Algospot/7장/쿼드 트리 뒤집기 - 22.04.08.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int C;
+
+string quad;
+
+string divAc(string str) {
+	if (str[0] != 'x') {
+		string ret;
+		ret.push_back(str[0]);
+		return ret;
+	}
+
+	string p1, p2, p3, p4;
+	int idx = 1;
+
+	p1 = divAc(str.substr(idx));
+	idx += p1.size();
+
+	p2 = divAc(str.substr(idx));
+	idx += p2.size();
+
+	p3 = divAc(str.substr(idx));
+	idx += p3.size();
+
+	p4 = divAc(str.substr(idx));
+
+	//cout << "p1 : " << p1 << "\n";
+	//cout << "p2 : " << p2 << "\n";
+	//cout << "p3 : " << p3 << "\n";
+	//cout << "p4 : " << p4 << "\n";
+
+	return "x" + p3 + p4 + p1 + p2;
+}
+
+int main() {
+	cin >> C;
+	while (C--) {
+		cin >> quad;
+
+		cout << divAc(quad) << "\n";
+	}
+
+}

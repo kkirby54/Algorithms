4759787c17fcf5f949bdd3d61ab320429deac340
kkirby54 <sdk926@gmail.com>
2022-09-15 22:23:31 +0900

    밀린 단계별 - DFS

---
+++ BOJ/그래프 - DFS.BFS/24480 - 알고리즘 수업 - 깊이 우선 탐색 2.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, M, R;
+int u, v;
+vector<int> adj[100000];
+int visited[100000];
+
+int cnt = 0;
+void dfs(int curr) {
+	visited[curr] = ++cnt;
+	
+	for (int next : adj[curr]) {
+		if (visited[next]) continue;
+		dfs(next);
+	}
+}
+
+int main() {
+	scanf("%d %d %d", &N, &M, &R);
+
+	for (int i = 0; i < M; i++) {
+		scanf("%d %d", &u, &v);
+		adj[u - 1].push_back(v - 1);
+		adj[v - 1].push_back(u - 1);
+	}
+	for (int i = 0; i < N; i++) {
+		std::sort(adj[i].begin(), adj[i].end(), std::greater<int>());
+	}
+
+	dfs(R - 1);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", visited[i]);
+	}
+
+}

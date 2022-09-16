c15e379c10713375d22960b533368995992f8fd4
kkirby54 <sdk926@gmail.com>
2022-09-16 23:32:16 +0900

    밀린 단계별 - BFS

---
+++ BOJ/그래프 - DFS.BFS/24445 - 알고리즘 수업 - 너비 우선 탐색 2.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+int N, M, R;
+int visited[100000];
+int u, v;
+std::vector<int> adj[100000];
+
+int cnt = 0;
+
+int main(){
+    scanf("%d %d %d", &N, &M, &R);
+
+    for (int i = 0; i < M; i++){
+        scanf("%d %d", &u, &v);
+        adj[u-1].push_back(v-1);
+        adj[v-1].push_back(u-1);
+    }
+    for (int i = 0; i < N; i++){
+        std::sort(adj[i].begin(), adj[i].end(), std::greater<int>());
+    }
+
+    std::queue<int> Q;
+    Q.push(R-1);
+    visited[R-1] = ++cnt;
+
+    while (!Q.empty()){
+        int curr = Q.front();
+        Q.pop();
+
+        for (int next : adj[curr]){
+            if (visited[next]) continue;
+            Q.push(next);
+            visited[next] = ++cnt;
+        }
+    }
+
+    for (int i = 0; i < N; i++){
+        printf("%d\n", visited[i]);
+    }
+
+}
